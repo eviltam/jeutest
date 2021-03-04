25594a92f32df9ede7f02b1183840e7349e1f9ce
eviltam <79590714+eviltam@users.noreply.github.com>
2021-03-04 09:56:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+
+const startButton = document.getElementById('start-btn')
+startButton.addEventListener('click', startGame)
+
+function startGame(){
+
+}
+
+function setNextQuestion(){
+    
+}
+class Question {
+    constructor(text, choices, answer) {
+      this.text = text;
+      this.choices = choices;
+      this.answer = answer;
+    }
+    isCorrectAnswer(choice) {
+      return this.answer === choice;
+    }
+  }
+  let questions = [
+    new Question("Quel est le nom du chien de Martine?", 
+    ["Patapouf", "Moustache", "Jean", "Croquette"], 
+    "Patapouf"),
+    new Question("Combien existe-t-il d'albums de Martine", 
+    ["20", "40", "60", "80"], 
+    "60"),
+  ];
+  
+  
+  class Quiz {
+    constructor(questions) {
+      this.score = 0;
+      this.questions = questions;
+      this.currentQuestionIndex = 0;
+    }
+    getCurrentQuestion() {
+      return this.questions[this.currentQuestionIndex];
+    }
+    guess(answer) {
+      if (this.getCurrentQuestion().isCorrectAnswer(answer)) {
+        this.score++;
+      }
+      this.currentQuestionIndex++;
+    }
+    hasEnded() {
+      return this.currentQuestionIndex >= this.questions.length;
+    }
+  }
+  
+  const display = {
+    elementShown: function(id, text) {
+      let element = document.getElementById(id);
+      element.innerHTML = text;
+    },
+    endQuiz: function() {
+      endQuizHTML = `
+        <h1>Quiz terminé !</h1>
+        <h3> Votre score est de : ${quiz.score} / ${quiz.questions.length}</h3>`;
+      this.elementShown("quiz", endQuizHTML);
+    },
+    question: function() {
+      this.elementShown("question", quiz.getCurrentQuestion().text);
+    },
+    choices: function() {
+      let choices = quiz.getCurrentQuestion().choices;
+  
+      guessHandler = (id, guess) => {
+        document.getElementById(id).onclick = function() {
+          quiz.guess(guess);
+          quizApp();
+        }
+      }
+      // affichage choix + prise en compte du choix
+      for(let i = 0; i < choices.length; i++) {
+        this.elementShown("choice" + i, choices[i]);
+        guessHandler("guess" + i, choices[i]);
+      }
+    },
+    progress: function() {
+      let currentQuestionNumber = quiz.currentQuestionIndex + 1;
+      this.elementShown("progress", "Question " + currentQuestionNumber + " sur " + quiz.questions.length);
+    },
+  };
+  
+  // Game logic
+  quizApp = () => {
+    if (quiz.hasEnded()) {
+      display.endQuiz();
+    } else {
+      display.question();
+      display.choices();
+      display.progress();
+    } 
+  }
+  // Create Quiz
+  let quiz = new Quiz(questions);
+  quizApp();
+  
